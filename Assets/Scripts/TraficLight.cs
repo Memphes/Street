b05765bde76c02228ab9b09301a1847e98cb6131
Memphes <Memphes20007@yandex.ru>
2022-08-30 01:18:26 +0300

    Script

---
+++ Assets/Scripts/TraficLight.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TrafficLight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("I'm starting as Light");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("I'm Light");
+    }
+}
